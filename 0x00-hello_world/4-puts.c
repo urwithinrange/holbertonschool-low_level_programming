fe04ba7f27ab5050702105b7a60739ab0c0543a3
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-04 02:49:10 +0000

    corrected for betty compliance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ *main - entry point
+ *
+ *Return: 0 success
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+return (0);
 }
