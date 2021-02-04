cd906abc7d3427cceed730f7db49e402e21028e9
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-04 02:58:11 +0000

    corrected the syntax for new linw

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-  *main - entry point
-  *
-  *Return: 0 on success
-  */
+ *main - entry point
+ *
+ *Return: 0 on success
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art"\n);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
