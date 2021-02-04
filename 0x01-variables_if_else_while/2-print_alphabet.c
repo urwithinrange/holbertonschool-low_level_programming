eb10a503f83a60ec862e13f0e46a222469ef8079
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-04 22:21:39 +0000

    Created file and wrote a program inside

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 on sucess
+  */
+int main(void)
+{
+	char a;
+	
+	for (a = 97; a <=122; a++)
+		putchar(a);
+	putchar('\n');
+return (0);
+}
