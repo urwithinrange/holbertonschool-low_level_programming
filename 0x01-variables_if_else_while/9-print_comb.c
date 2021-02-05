d1dcf9897410c2a8e6dbc84a24512c8dd81c4bf5
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-05 04:05:33 +0000

    Corrected betty errors

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,17 +2,17 @@
 /**
   *main - Entry point
   *
-  *Retrun: 0 on success
+  *Return: 0 on success
   */
 int main(void)
 {
 	int n;
 
-	for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + 48);
 		if (n < 9)
-		{	
+		{
 			putchar(',');
 			putchar(' ');
 		}
