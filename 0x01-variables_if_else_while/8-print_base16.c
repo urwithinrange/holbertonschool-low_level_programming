bbd96e8e72c70323876439784d8385f77b398a29
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-05 03:46:01 +0000

    created file and wrote program in it

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 if succesful
+  */
+int main(void)
+{
+	char a =  48, b = 97;
+
+	for (; a <= 57; a++)
+		putchar(a);
+	for (; b <= 102; b++)
+		putchar(b);
+	putchar('\n');
+return (0);
+}
