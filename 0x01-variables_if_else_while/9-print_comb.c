210a525291b919b08e2efd9f885db3324a65af3a
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-05 03:58:11 +0000

    Created file and wrote program into it

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Retrun: 0 on success
+  */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+		if (n < 9)
+		{	
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
