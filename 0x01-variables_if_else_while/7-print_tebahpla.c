a2534ef3a40abfae467fdf8fc7cc8a3aa8a3326d
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-05 02:39:18 +0000

    created file and wrote program in it

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 on success
+  */
+int main(void)
+{
+	char a;
+
+	for (a = 122; a >= 97; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
