8e4a563ef23650f693edc6e9164d97f7330d3e56
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-08 16:38:09 +0000

    Created file and wrote program into it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  *main - Entry point
+  *Description - use _putchar to print alphabet
+  *Return: 0 upon sucess
+  */
+int main(void)
+{
+	char a;
+	
+	for(a = 97; a <= 122; a++)
+		_putchar(a);
+	_putchar('\n');
+	return (0);
+}
