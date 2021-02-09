308754d218c0e4e7f0ff06cfd201c7f8ee8a2881
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-09 15:30:11 +0000

    Corrected the betty format

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *print_alphabet_x10 - 10 times the alphabet, in lowercase, followed by a new line
+ *print_alphabet_x10 - 10 times the alphabet, in lowercase, followed by \n
  *Description - use _putchar to print alphabet
  *Return: void
  */
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char c;
 	int n;
 
-	for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		c = 'a';
 		for (c = 'a'; c <= 'z'; c++)
