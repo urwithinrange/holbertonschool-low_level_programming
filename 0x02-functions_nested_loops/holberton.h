0af6895b9fdee33a872f6bd397bf1c93fdd9d205
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-09 05:40:22 +0000

    updated system calls

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,11 +6,11 @@ int _putchar(int c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int n);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 
