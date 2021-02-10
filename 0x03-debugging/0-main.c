10bcb6fdab03c8770d9d977405ad03e4f11c7441
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-10 02:53:39 +0000

    checking comment redaction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-  * main - tests function that prints if integer is pos, neg, or 0
+  * main - tests function that prints if integer is positive or negative
   * Return: 0
   */
 
