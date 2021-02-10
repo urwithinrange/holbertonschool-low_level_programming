7e982cf98846afbbf12736c2b2c68b730cdceebc
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-10 02:49:02 +0000

    Created file and wrote in program

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * main - tests function that prints if integer is pos, neg, or 0
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
