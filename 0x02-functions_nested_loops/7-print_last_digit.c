44debe8cbe1f5b5843c636aafaae738377c1d2b6
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-10 04:43:18 +0000

    Greatly simplified function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,31 +1,15 @@
 #include "holberton.h"
 /**
-  *_abs - the absolute value of a number
-  *Description - returns the absolute number
-  *@n: the number being passed
-  *Return: n as the absolute number
-  */
-int _abs(int n)
-{
-	if (n < 0)
-	{
-		n *= -1;
-		return (n);
-	}
-	else
-		return (n);
-}
-
-/**
   *print_last_digit -function that prints the last digit of a number
   *@n: the number being passed
   *Return: the last digit of the number
   */
 int print_last_digit(int n)
 {
-	int av = _abs(n);
 
-	av = av % 10;
-	_putchar(av + '0');
-	return (av);
+	if (n < 0)
+		n = (n - (n * 2));	
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
 }
