7cefdbc0e7183242771772b60beb26fbacfbc3c4
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-11 21:20:32 +0000

    Created file and wrote program into it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  *mul - function that multiplies 22  numbers
+  *@a: first number for equasion
+  *@b: second number for equasion
+  *Return: n which is the product of mul
+  */
+int mul(int a, int b)
+{
+	int n = a * b;
+
+	return (n);
+}
+
