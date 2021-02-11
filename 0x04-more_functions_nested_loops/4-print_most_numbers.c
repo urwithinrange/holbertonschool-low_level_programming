fde022c2cb8c89fca171750d65a12fc4b1b09761
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-11 21:54:35 +0000

    Created file and wrote program into it

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  *print_most_numbers - function that prints the numbers, 0,1,3,4-9
+  *
+  *Return: 0 on success
+  */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 || n != 4)
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
