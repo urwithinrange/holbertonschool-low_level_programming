42d478f8cb168db16ddcc44d28656dad61eca59a
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-11 23:50:14 +0000

    Created file and wrote probram into it

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  *main - Entry point
+  *Description: prints from 0 - 100 and fizz buzz is used
+  *Return: 0 on sucess
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
