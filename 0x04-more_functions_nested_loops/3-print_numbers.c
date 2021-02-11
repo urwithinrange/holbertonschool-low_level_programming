ec631d662990869ded5e19c537734dd7343820d9
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-11 21:34:55 +0000

    Created file and wrote program into it

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  *print_numbers -  function that prints the numbers, from 0 to 9
+  *
+  *Return: void
+  */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + '0');
+	_putchar('\n');
+}
