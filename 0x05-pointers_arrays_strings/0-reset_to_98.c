58fb56d1de0bab759dbbe034da3e99f81346932f
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-16 21:02:38 +0000

    Created file and wrote program into it

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  *reset_to_98 - function that takes a pointer as a parameter and updates
+  *@n: the pointer being referenced
+  *Return: void
+  */
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p = 98;
+}
