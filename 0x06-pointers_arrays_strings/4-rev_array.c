f7d0122d57889ff59ce2e7a3ddcdc413eb226dfb
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-18 21:49:06 +0000

    Created file and wrote program into it

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *reverse_array - reverses an array of integers
+  *@a: the array holding the numbers
+  *@n: the total number of int
+  *Return: void
+  */
+void reverse_array(int *a, int n)
+{
+	int temp, i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
