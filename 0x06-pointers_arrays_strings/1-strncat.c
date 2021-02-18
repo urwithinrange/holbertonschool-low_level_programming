aaa97fb97c11d81102ee3a588410eceb92cc1518
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-18 19:12:50 +0000

    Created file and wrote program into it

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *_strncat -  concatenates two strings
+  *@dest: the original string for base
+  *@src: the string to be added
+  *@n: bytes used from src
+  *
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen, i;
+
+	for (dlen = 0; dest[dlen] != '\0'; dlen++)
+		continue;
+	for (i = 0; i < n && src[i] != '\0'; dlen++, i++)
+		dest[dlen] = src[i];
+	dest[dlen] = '\0';
+	return (dest);
+}
