405886d44f78f48ab5bc1dbe5c2af474ea720a4d
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-19 03:33:25 +0000

    Created file and wrote program into it

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  **rot13 - function that encodes a string using rot13
+  *@str: the string being passed
+  *Return: the modded string
+  */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (; (str[i] >= 97 && str[i] <= 122) ||
+				(str[i] >= 65 && str[i] <= 90); i++)
+		{
+			if (str[i] >= 78 && str[i] <= 90)
+				str[i] -= 13;
+			else if (str[i] >= 110 && str[i] <= 122)
+				str[i] -= 13;
+			else
+				str[i] += 13;
+		}
+	}
+	return (str);
+}
