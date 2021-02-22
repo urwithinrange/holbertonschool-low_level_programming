a8cdf6c6b0899a78f614e44aed180214c373c4a8
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-22 19:49:10 +0000

    check for null byte

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, n = 0;
 	char *p = accept;
 
-	for (i = 0; s[i] != 44; i++)
+	for (i = 0; s[i] != 44 && s[i] != '\0'; i++)
 		for (j = 0; p[j] != 00; j++)
 			if (p[j] == s[i])
 				n++;
