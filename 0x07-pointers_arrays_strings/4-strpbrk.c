43c0587b2d89b0ba5f5ec9fcb95fb5191ebd79a0
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-22 19:45:53 +0000

    Created file and wrote program into it

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  **_strpbrk - searches a string for any of a set of bytes.
+  *@s: the string searched
+  *@accept: the string of char bytes to stop at
+  *Return: the mod string
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+	
+	for (; *s != '\0'; s++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (*s == accept[j])
+				return (s);
+	return (NULL);
+}
