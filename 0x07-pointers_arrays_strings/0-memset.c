ba394193d3d296aa23cb5a3e1fb30db1dd9dc02b
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-22 17:16:34 +0000

    Corrected for betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
   **_memset - fills memory with a constant byte
-  *@s: the memory area pointed to 
+  *@s: the memory area pointed to
   *@b: the constant to be used for filling memory
   *@n: number of bytes in memory
   *Return: the mod memory
   */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; 
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
