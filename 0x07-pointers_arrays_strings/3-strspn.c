498e92661db9a93b0bfb2c2c08df655fbba97478
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-22 19:28:32 +0000

    Created file and wrote program into it

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *_strspn - gets the length of a prefix substring
+  *@s: the string passed
+  *@accept: the bytes counted
+  *Return: the numnber of bytes counted
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, n = 0;
+	char *p = accept;
+
+	for (i = 0; s[i] != 44; i++)
+		for (j = 0; p[j] != 00; j++)
+			if (p[j] == s[i])
+				n++;
+	return (n);
+}
