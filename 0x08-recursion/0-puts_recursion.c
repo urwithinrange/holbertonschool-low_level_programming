44359588123393f1a70c783ac50d0ee772a2d3f7
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-24 22:16:41 +0000

    Created file and wrote program into it

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *_puts_recursion - prints a string followed by a new line
+  *@s; the string passed
+  *
+  *Return: void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == 00)
+		_putchar('\n');		
+	
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+}
