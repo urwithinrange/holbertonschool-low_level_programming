90dcba7c7c0ff83716fc31abeaf022d0e317a156
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-24 22:18:09 +0000

    Corected syntax

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
   *_puts_recursion - prints a string followed by a new line
-  *@s; the string passed
+  *@s: the string passed
   *
   *Return: void
   */
 void _puts_recursion(char *s)
 {
 	if (*s == 00)
-		_putchar('\n');		
-	
+		_putchar('\n');
+
 	if (*s)
 	{
 		_putchar(*s);
