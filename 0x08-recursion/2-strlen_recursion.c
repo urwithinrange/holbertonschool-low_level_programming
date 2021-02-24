06749caf494a53cf784bbf0320a5a3cb85c71115
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-24 23:25:16 +0000

    Created file and wrote program into it

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  *_strlen_recursion - return the length of a string
+  *@s: the string
+  *
+  *Return: the length of the string
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + (_strlen_recursion(s + 1)));
+}
