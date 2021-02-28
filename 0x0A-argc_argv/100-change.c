0988802add12dea4ad7f4eeed134120adc53ff37
Derek_Clemens <urwithinrange@yahoo.com>
2021-02-28 21:09:38 +0000

    corrected to account for neg numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,19 +17,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	t = atoi(argv[1]);
-	for (; t != 0; c++)
+	if	(t < 1)
 	{
-		if (t >= 25)
-			t -= 25;
-		else if (t >= 10)
-			t -= 10;
-		else if (t >= 5)
-			t -= 5;
-		else if (t >= 2)
-			t -= 2;
-		else
-			t -= 1;
+		printf("0\n");
+		return (0);
 	}
+	for (; t != 0; c++)
+		{	
+			if (t >= 25)
+				t -= 25;
+			else if (t >= 10)
+				t -= 10;
+			else if (t >= 5)
+				t -= 5;
+			else if (t >= 2)
+				t -= 2;
+			else
+				t -= 1;
+		}
 	printf("%d\n", c);
 	return (0);
 }
