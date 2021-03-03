6f002644d2c682a5cdf10d9ea53ad970e6bf9567
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-03 19:55:29 +0000

    changed to model an example on geeks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,28 +7,22 @@
   */
 int **alloc_grid(int width, int height)
 {
-	int **buffer, i, j;
+	int **buffer, *ptr, i, j, size;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	buffer = malloc(sizeof(int *) * height);
+	size = (sizeof(int*) * height) + (sizeof(int) * height * width);
+	buffer = (int **)malloc(size);
 	if (buffer == NULL)
 		return (NULL);
+	/* points to the first element of a 2D array*/
+	ptr = (int *)(buffer + height);
+	/*loop to point height pointer to appropriate location in 2D array*/
+	for(i = 0; i < height; i++)
+		buffer[i] = (ptr + width * i); 
+	/*loop sets buffer to 0*/
 	for (i = 0; i < height; i++)
-	{
-		buffer[i] = malloc(width * sizeof(int));
-			if (buffer[i] == NULL)
-			{
-				for (; i > 0; i--)
-					free(buffer[i]);
-				free(buffer);
-				return (NULL);
-			}
-	}
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			buffer[i][j] = 0;
-	}
 	return (buffer);
 }
