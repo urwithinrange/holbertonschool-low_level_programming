c7a6a7b3ddb111ef8e9a604e6dfbefe86ed80660
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-03 02:35:57 +0000

    Removed add null byte

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,28 +22,18 @@ int _strlen(char *s)
   */
 char *str_concat(char *s1, char *s2)
 {
-	int len_s1, len_s2, len, i, n = 0;
+	int len_s1, len_s2, len, i, n;
 	char *cat;
 
 	len_s1 = _strlen(s1);
 	len_s2 = _strlen(s2);
 	len = len_s1 + len_s2;
-	len_s1--;
-	len_s2--;
 	cat = malloc(sizeof(char) * (len + 1));
 	if (cat == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
-	{
-		if (i <= len_s1)
-			cat[i] = s1[i];
-		else if (i <= len)
-		{
-			cat[i] = s2[n];
-			n++;
-		}
-		else
-			cat[i] = 00;
-	}
+	for (i = 0; s1[i] != 00; i++)
+		cat[i] = s1[i];
+	for (n = 0; s2[n] != 00; n++)
+		cat[n + len_s1] = s2[n];
 	return (cat);
 }
