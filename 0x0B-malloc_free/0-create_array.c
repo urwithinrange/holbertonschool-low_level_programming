a074a27ed283b7905472a97175c22fff3e75bce1
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-03 00:16:31 +0000

    Created file and wrote program into it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+  **create_array - creates an array of chars, and initializes a specific char
+  *@size: the length of the array
+  *@c: the character being passed
+  *Return: pointer to array of NULL if fail or 0
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+	char *p;
+
+	p = malloc((size + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+	for (n = 0; n < size; n++)
+		p[n] = c;
+	p[n] = '\n';
+	return (p);
+}
