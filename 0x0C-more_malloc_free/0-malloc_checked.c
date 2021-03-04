f240b9a09271922518e2d3093d37aa555d151d03
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-04 18:59:24 +0000

    Created file and wrote program into it

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *malloc_checked - allocates memory using malloc
+  *@b: the amount of memory to be allocated
+  *
+  *Return: void
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *p =(size_t *)malloc(b);
+
+	if (p == NULL)
+	{
+		free(p);
+		exit (98);
+		}
+	return (p);
+}
