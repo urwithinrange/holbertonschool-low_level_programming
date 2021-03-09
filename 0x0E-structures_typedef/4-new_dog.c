cbb3df84d785045ee068e8f3b8cb3a39cc673238
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-09 02:48:08 +0000

    Trying to create strings to store values

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,20 @@
 #include "dog.h"
 /**
+ *_strlen - function that returns the length of a string
+ *@s: the string passed
+ *Return: 0 on sucess
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	if (s == NULL)
+		return (0);
+	for (n = 0; s[n] != '\0'; n++)
+		continue;
+	return (n);
+}
+/**
   *new_dog - creating a new dog
   *@name: dog name
   *@age: dog age
@@ -8,6 +23,7 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nlen, olen, i;
 	dog_t *d;
 
 	if (name == NULL || owner == NULL)
@@ -15,8 +31,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	nlen = _strlen(name);
+	olen = _strlen(owner);
+	d->name = malloc(nlen * sizeof(char));
+	if (d->name == NULL)
+		return (NULL);
+	d->owner = malloc(olen * sizeof(char));
+	if (d->owner == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		d->name[i] = name[i];
 	d->age = age;
-	d->owner = owner;
+	for (i = 0; i < olen; i++)
+		d->owner[i] = owner[i];
 	return (d);
 }
