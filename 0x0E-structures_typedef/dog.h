3986ec2e7d358a8b9aff0c25db9e5e9b236b3ef9
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-09 01:58:22 +0000

    removed the junk

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t
-{
-	char *name;
-	char *owner;
-	float age;
-};
+typedef struct dog dog_t;
 /* dog_t *new_dog(char *name, float age, char *owner); */
 /*void free_dog(dog_t *d);*/
 #endif
