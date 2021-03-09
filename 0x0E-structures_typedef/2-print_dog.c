3f0c610349921059393fbe97c43b8c0207e81603
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-09 01:13:39 +0000

    Direct printing of nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,17 +6,19 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		if (d->name == NULL)
-			d->name = "(nil)";
-		if (d->owner == NULL)
-			d->owner = "(nil)";
-		printf("Name: %s\n", d->name);
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 		if (d->age == (00 || '\0'))
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
