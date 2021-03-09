48e6376854e7400e6250255ab528bead17290158
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-09 02:30:25 +0000

    Update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,10 @@ struct dog
 	float age;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
-/* dog_t *new_dog(char *name, float age, char *owner); */
-/*void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
