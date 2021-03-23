baf37c9b22602bef5b283e07b7e464e7a8676c23
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-23 21:41:56 +0000

    created file and wrote program into it

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  *free_listint2 - frees a list from memory
+  *@head: the address of the list
+  *Return: void
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *next = NULL, *ptr;
+
+	if (*head == NULL)
+		return;
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		next = ptr->next;
+		free(ptr);
+		ptr = next;
+	}
+	free(ptr);
+	free(next);
+}
+		
