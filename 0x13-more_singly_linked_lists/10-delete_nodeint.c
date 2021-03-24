97a5d6189ef67aeeeb33822ed2f0dece4aac50d3
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-24 04:33:20 +0000

    Checking to see that ptr exist

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(ptr);
 		return (1);
 	}
-	for (i = 1; i < index; i++)
+	for (i = 1; (ptr) && i < index; i++)
 	{
 		temp = ptr->next;
 		if (i + 1 == index)
