f899ba3586cac01936d2ea7f2fdb149d5a89fd7c
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-24 04:30:05 +0000

    Created a comipleing code

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+  *delete_nodeint_at_index - delete a specific node at index
+  *@head: the list being modded
+  *@index: the node index to delete
+  *Return: 1 if success or -1 for fail
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *ptr, *temp;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	ptr = *head;
+	if (index == 0)
+	{
+		*head = ptr->next;
+		free(ptr);
+		return (1);
+	}
+	for (i = 1; i < index; i++)
+	{
+		temp = ptr->next;
+		if (i + 1 == index)
+		{
+			ptr->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		ptr = temp;
+	}
+	return (-1);
+}
