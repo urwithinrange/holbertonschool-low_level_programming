ceb2f3c6da955660891225fca54dc5f0d7fe9081
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-26 02:52:52 +0000

    created file and wrote program into it

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * get_bit - returns the value at a given index
+  *@n: the number to be indexed thru
+  *@index: the index of the number, starting from 0 of the bit to get
+  *Return: the value of the bit at the index index or -1 for error
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (index > n)
+		return (-1);
+	/*convert n to binary*/
+	i = (n >> index) & 1;
+	return (i);
+}
