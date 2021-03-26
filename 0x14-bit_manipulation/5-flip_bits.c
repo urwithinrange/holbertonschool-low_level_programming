622c646b34ec4eb0eecd35dbd5be77a7b6d16fd0
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-26 04:17:28 +0000

    created file and wrote program into it

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  *flip_bits - function returns the number of bits from A to B
+  *@n: the first number to compare
+  *@m: the second numnber to compare
+  *Return: the number of numbers that would have to flip to convert
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = 0, count;
+
+	i = (n ^ m);
+	for (count = 0; i > 0; count++)
+		i &= (i - 1);
+	return (count);
+}
