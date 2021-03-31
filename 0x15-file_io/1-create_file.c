4942966275b2b64bff4b4913b177f2db4297f4e4
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:49:55 +0000

    Corrected a typo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	file_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (field == -1)
 		return (-1);
-	write(fil_des, text_content, len);
+	write(file_des, text_content, len);
 	close(file_des);
 	return (1);
 }
