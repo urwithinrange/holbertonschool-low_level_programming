ec85d2c1d093f864d47d6ed94a39c35883b6e95f
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:59:29 +0000

    Rearranged to check for NULL string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,13 +12,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	for (len = 0; text_content[len] != 00; len++)
-		continue;
 	file_des = open(filename, O_RDWR | O_APPEND);
-	if (file_des == -1) 
+	if (file_des == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (1);
+		return (-1);
+	for (len = 0; text_content[len] != 00; len++)
+		continue;
 	write(file_des, text_content, len);
 	return (1);
 }
