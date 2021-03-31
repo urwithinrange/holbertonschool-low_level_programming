4aa5310bf01b2c03bdbb51c99a75ac8ac39c6539
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:46:14 +0000

    created file and wrote program into it

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  *append_text_to_file - appends text at the end of a file
+  *@filename: the name of the file
+  *@text_content: ios the string  to add at the end of the file
+  *
+  *Return: 1 on success and -1 on failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len, file_des;
+
+	if (filename == NULL)
+		return (-1);
+	for (len = 0; text_content[len] != 00; len++)
+		continue;
+	file_des = open(filename, O_APPEND | O_RDWR);
+	if (file_des == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	write(file_des, text_content, len);
+	return (1);
+}
