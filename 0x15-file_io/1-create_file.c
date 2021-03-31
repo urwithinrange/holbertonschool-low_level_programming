0ab4568acdaf377f3209778f01cd93c10cc50253
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:51:41 +0000

    Corrected if st

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	len = _strlen(text_content);
 	file_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (field == -1)
+	if (file_des == -1)
 		return (-1);
 	write(file_des, text_content, len);
 	close(file_des);
