f8a2209778a8bc187850a7219f52debf1df1e629
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:53:23 +0000

    Swapped the location of fd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,8 +14,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	for (len = 0; text_content[len] != 00; len++)
 		continue;
-	file_des = open(filename, O_APPEND | O_RDWR);
-	if (file_des == -1)
+	file_des = open(filename, O_RDWR | O_APPEND);
+	if (file_des == -1) 
 		return (-1);
 	if (text_content == NULL)
 		return (1);
