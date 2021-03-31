8b80d607d3ba16e5433961a21e9fbd89e552a950
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-31 03:45:55 +0000

    created file and wrote program into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ *_strlen - function that returns the length of a string
+ *@s: the string passed
+ *Return: 0 on sucess
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	if (s == NULL)
+		return (0);
+	for (n = 0; s[n] != '\0'; n++)
+		continue;
+	return (n);
+}
+/**
+  *create_file - function that creates a file
+  *@filename: the name of the file to create
+  *@text_content: is a NULL terminated string to write to the file
+  *Return: 1 on success and -1 on fail
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int len, file_des;
+
+	if (filename == NULL)
+		return (-1);
+	len = _strlen(text_content);
+	file_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (field == -1)
+		return (-1);
+	write(fil_des, text_content, len);
+	close(file_des);
+	return (1);
+}
