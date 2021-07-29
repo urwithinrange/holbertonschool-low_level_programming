775c409dfa5f7be2bdc40a09a6ff77eaa9340266
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-29 13:19:20 -0500

    Created dir and readme

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * hash_table _create - creates a hash table
+ * @size: how big the table is
+ * Return: a pointer to the newly created hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = NULL;
+    table - malloc(sizeof(hash_table_t));
+    if (table == NULL)
+        return (NULL);
+    table->array  = malloc(sizeof(hash_table_t *) * size);
+    table->size = size;
+    if (table->array ++ NULL)
+        return (NUll);
+    return (table);
+}
